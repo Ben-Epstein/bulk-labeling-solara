a41f8d09b2587985b3352ddc0f82c59636f7c98f
Ben Epstein <ben.epstein97@gmail.com>
2023-01-18 17:44:14 -0500

    embeddings selection and reset

--- bulk-labeling/main.py
+++ bulk-labeling/main.py
@@ -1,29 +1,41 @@
-import solara
-import plotly.express as px
-# import solara.express as px
-import pandas as pd
-from typing import cast, Optional, List
+import os
+from typing import Optional, cast
 
-PATH = "conv_intent.csv"
+import pandas as pd
+import plotly.express as px
+import solara
+
+DIR = f"{os.getcwd()}/bulk-labeling"
+PATH = f"{DIR}/conv_intent.csv"
 
 
 @solara.component
-def dataframe_view(df: pd.DataFrame, point_size: int, color: str) -> solara.HBox:
+def dataframe_view(
+    df: pd.DataFrame, point_size: int, color: str, hard_reset: bool = False
+) -> solara.HBox:
+    selection_data, set_selection_data = solara.use_state(None)
+    if hard_reset:
+        set_selection_data(None)
+    if selection_data:
+        df = df[df["id"].isin(selection_data["points"]["point_indexes"])]
     with solara.HBox() as main:
         df_col = solara.VBox()
         embs_col = solara.VBox()
 
         with df_col:
+            solara.Markdown("## Data")
             solara.DataFrame(df)
         with embs_col:
+            solara.Markdown("## Embeddings")
             p = px.scatter(df, x="x", y="y", color=color or None)
             p.update_layout(showlegend=False)
             p.update_xaxes(visible=False)
             p.update_yaxes(visible=False)
 
             p.update_traces(marker_size=point_size)
-            solara.FigurePlotly(p)
+            solara.FigurePlotly(p, on_selection=set_selection_data)
     return main
+
 
 @solara.component
 def no_df():
@@ -43,6 +55,7 @@ def Page():
     df, set_df = solara.use_state(
         cast(Optional[pd.DataFrame], None), eq=lambda *args: False
     )
+    hard_reset, set_reset = solara.use_state(False)
     point_size, set_point_size = solara.use_state(2)
 
     color, set_color = solara.use_state("")
@@ -50,9 +63,6 @@ def Page():
 
     def update_chosen_color(new_color: str):
         set_color(new_color)
-
-    # def update_avl_cols(new_cols: List[str]):
-    #     set_cols(new_cols)
 
     def update_point_size(new_point_size: int):
         set_point_size(new_point_size)
@@ -63,7 +73,10 @@ def Page():
         set_cols(list(new_df.columns))
 
     def reset():
-        set_df(None)
+        # Force the child page (dataframe view) to reset selected points
+        set_reset(True)
+        # Set it back to False in order to preserve future lasso selections
+        set_reset(False)
 
     main = solara.HBox()
 
@@ -71,15 +84,13 @@ def Page():
         solara.Title("Bulk Labeling!")
         with solara.VBox():
             solara.Button(label="Load demo dataset", on_click=load_df)
-            solara.Button(label="Reset", on_click=reset)
-            solara.SliderInt(
-                "Set embedding point size", point_size, on_value=update_point_size
-            )
-            solara.Select(
-                "Color embeddings by column", "", avl_cols, on_value=update_chosen_color
-            )
+            solara.Button(label="Reset view", on_click=reset)
+            solara.Markdown("**Set point size**")
+            solara.SliderInt("", point_size, on_value=update_point_size)
+            solara.Select("Color by", "", avl_cols, on_value=update_chosen_color)
         if df is not None:
-            dataframe_view(df, point_size, color)
+            df["id"] = list(range(len(df)))
+            dataframe_view(df.copy(), point_size, color, hard_reset)
         else:
             no_df()
     return main
